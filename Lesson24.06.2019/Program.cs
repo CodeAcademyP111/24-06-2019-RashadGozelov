c4a0e8cfa80247f95fa8b31e815e462fbb7e5848
RashadGozelov <49427442+RashadGozelov@users.noreply.github.com>
2019-06-24 07:06:42 -0700

    task

---
+++ Lesson24.06.2019/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson24._06._2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sentence = "Salam Sirvan";
+            StringBuilder lastResult = Word(sentence);
+            Console.WriteLine(lastResult);
+
+        }
+
+
+        static StringBuilder Word(string words)
+        {
+            string[] word = words.Split(' ');
+            StringBuilder result = new StringBuilder();
+            foreach (var item in word)
+            {
+                if (word[0] != item)
+                {
+                    result.Append(" ");
+                }
+                for ( int i=item.Length-1; i>=0; i-- )
+                {
+                    result.Append(item[i]);
+                }              
+            }
+             return result;
+        }
+    }
+}
